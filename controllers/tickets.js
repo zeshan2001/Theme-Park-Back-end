d1b2c9abd082bf2ba97b8481616eb0325d71fd72
Abdulla-Zaid <abdulla4433@gmail.com>
2025-08-05 00:35:16 +0300

    done with create ticket and delete ticke in controller and routes

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,1 +1,34 @@
 const Tickets = require('../models/ticket')
+
+const Find_TicketID_get = async (req, res) => {
+  try {
+    const ticket = await Tickets.findById(req.params.ticketid)
+    res.status(200).send(ticket)
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to get ticket' })
+  }
+}
+
+const create_Ticket_post = async (req, res) => {
+  try {
+    const ticket = await Tickets.create({ ...req.body })
+    res.status(200).send(ticket)
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to create ticket' })
+  }
+}
+
+const delete_Ticket_delete = async (req, res) => {
+  try {
+    const ticket = await Tickets.findByIdAndDelete(req.params.ticketid)
+    res.status(200).send(ticket)
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to delete ticket' })
+  }
+}
+
+module.exports = {
+  Find_TicketID_get,
+  create_Ticket_post,
+  delete_Ticket_delete
+}
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,8 @@
 const router = require('express').Router('../controllers/tickets')
 const ticketsCTRL = require('../controllers/tickets')
-// router.get('/:ticketid', null)
-// router.post('/new', null)
-// router.delete('/:ticketid', null)
+
+router.get('/:ticketid', ticketsCTRL.Find_TicketID_get)
+router.post('/new', ticketsCTRL.create_Ticket_post)
+router.delete('/:ticketid', ticketsCTRL.delete_Ticket_delete)
 
 module.exports = router
