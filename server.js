ce546e63bada50c7aa928cde526adf96f4093aa4
zeshan2001 <zeshanbh@gmail.com>
2025-08-05 17:30:31 +0300

    update schema

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const logger = require('morgan')
 const Rides = require('./routes/rides')
 const Tickets = require('./routes/tickets')
+const cors = require('cors')
 const PORT = process.env.PORT || 3001
 const db = require('./db/db')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
